629d33a11d8c239c6ad3fae385cc4132def48a47
Eslam <eslamnsr@hotmail.com>
2020-11-15 06:42:57 +0200

    imported datasets

--- server.R
+++ server.R
@@ -1,1 +1,18 @@
-server <- function(input,output) {}
+server <- function(input,output) {
+  
+  output$provider.hist <- renderTable(provider.dataset)
+  
+  output$payer.subtitle <- renderText({
+    val <- subset()
+  })
+  
+  output$payer.info <- renderValueBox({
+    valueBox(
+      paste0(subset(payer.dataset$claim.cost,payer.dataset$name==input$payer.input)), 
+      paste0(payer.input),
+      icon = icon("list"),
+      color = "purple"
+    )
+  })
+
+}
