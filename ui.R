9ee27138248ebce77915d7fe1afaf67267f08014
Eslam <eslamnsr@hotmail.com>
2020-11-15 06:43:45 +0200

    developed ui

--- ui.R
+++ ui.R
@@ -1,13 +1,33 @@
 library(shiny)
 library(shinydashboard)
 
+read.csv('provider.dataset.csv')
+read.csv('payer.dataset.csv')
+
+
 dashboardPage(
   dashboardHeader(title = "Claims Insights"),
   dashboardSidebar(
     sidebarMenu(
       menuItem('Dashboard', tabName = 'dashboard'),
-      menuItem('Provider', tabName = 'Provider')
+      menuItem('Provider', tabName = 'provider'),
+      menuItem('Payer', tabName = 'payer')
     )
-    ),
-  dashboardBody('this is a demo')
+  ),
+  dashboardBody(
+    #dashboard tab content
+    tabItem(tabName = 'dashboard',
+            fluidRow(
+              column(4,
+                     selectInput('payer.input',
+                                 label = 'select the payer',
+                                 choices =  payer.dataset$name),
+                     valueBoxOutput('payer.info')
+                    ),                   
+              column(12,tableOutput('provider.hist'))
+            )
+            ),
+    #providers tab content
+    tabItem(tabName = 'provider')
+  )
 )
