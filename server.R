16ceddea431106f69d9262f31cbfb1acd77ac28d
Eslam <eslamnsr@hotmail.com>
2020-11-19 06:09:07 +0200

    developed providers histogram

--- server.R
+++ server.R
@@ -1,18 +1,9 @@
 server <- function(input,output) {
   
-  output$provider.hist <- renderTable(provider.dataset)
+  output$provider.table <- renderTable(provider.dataset)
   
-  output$payer.subtitle <- renderText({
-    val <- subset()
-  })
-  
-  output$payer.info <- renderValueBox({
-    valueBox(
-      paste0(subset(payer.dataset$claim.cost,payer.dataset$name==input$payer.input)), 
-      paste0(payer.input),
-      icon = icon("list"),
-      color = "purple"
-    )
-  })
-
+  output$provider.plot <- renderPlot(
+    ggplot(provider.dataset,aes(x=claim.rate))+
+      geom_histogram()
+  )
 }
